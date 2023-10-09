301c4277441a721cc636f0b8f4dcf48283598774
Yahav Corcos <110262882+ycorcos1@users.noreply.github.com>
2023-10-09 18:56:03 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { createContext, useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Results from "./pages/Results";
+import BreakingNews from "./pages/BreakingNews";
+
+export const AppContext = createContext();
+
+const BREAKING_URL = `https://newsapi.org/v2/top-headlines?apiKey=a78f98488ea0469e8dab62abe7963279&country=us`;
+const API_URL =
+  "https://newsapi.org/v2/everything?apiKey=a78f98488ea0469e8dab62abe7963279";
+
+function App() {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [articles, setArticles] = useState([]);
+
+  const getBreakingNews = async () => {
+    const response = await fetch(`${BREAKING_URL}`);
+    const data = await response.json();
+    console.log("REQUEST MADE");
+    setArticles(data.articles);
+  };
+
+  const searchKeywordArticles = async () => {
+    const response = await fetch(
+      `${API_URL}&q=${searchKeyword}&sortBy=popularity`
+    );
+    const data = await response.json();
+    setArticles(data.articles);
+    console.log(articles);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        searchKeyword,
+        setSearchKeyword,
+        articles,
+        setArticles,
+        getBreakingNews,
+        searchKeywordArticles,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/keywordarticles" element={<Results />} />
+        <Route path="/breakingnews" element={<BreakingNews />} />
+      </Routes>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
